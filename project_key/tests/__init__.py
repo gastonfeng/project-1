3933d04ec07e79d036187fdc52ef64a65906e70e
gastonfeng <gastonfeng@gmail.com>
2019-03-10 17:54:13 +0800

    test project cost

--- project_key/tests/__init__.py
+++ project_key/tests/__init__.py
@@ -1,3 +1,3 @@
-from . import test_controller
 from . import test_project
 from . import test_task
+from . import test_controller
--- project_timeline_critical_path/models/__init__.py
+++ project_timeline_critical_path/models/__init__.py
@@ -1,4 +1,4 @@
-from . import project_project
-from . import project_task
 from . import res_company
 from . import res_config_settings
+from . import project_task
+from . import project_project
